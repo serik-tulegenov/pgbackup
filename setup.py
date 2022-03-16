f1f06ea82a54e5686d8c052f8d1a2eee5be768a4
Serik <tulegenoff.s@gmail.com>
2022-03-16 15:10:33 +0000

    Writeing the pieces

--- setup.py
+++ setup.py
@@ -11,6 +11,10 @@ setup(
     description = 'A utility for backing up PostgreSql databases',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
-    url = 'https://github.com/your_username/pgbackup',
-    packages=find_packages('src')
-)
+    url = 'https://github.com/serik-tulegenov/pgbackup',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    install_requires = ['boto3'],
+    entry_points={
+        'console_scripts': ['pgbackup = pgbackup.cli:main', ], }
+    )
